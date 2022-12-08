2e079d76b60026c815b2aa64af8c2d557698a12c
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2022-12-08 23:49:14 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  main - main block
- *  Return: 0 
- */
-int main(void)
+*  main - main block
+*  Return: 0 
+*/This is a simple example of how to use the libevent API.
+        int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
         return (0);

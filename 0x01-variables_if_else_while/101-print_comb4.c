6bf6cf23496f00b6f8e2c5877849320935f30c6a
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2022-12-10 01:41:09 +0300

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+  int hundreds = '0';
+
+	for (hundreds = '0' ; hundreds <= '9' ; hundreds++) /*prints tens digit*/
+	{
+    for (tens = '0' ; tens <= '9' ; tens++) /*prints tens digit*/
+	{
+		for (ones = '0' ; ones <= '9' ; ones++) /*prints ones digit*/
+		{
+			if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))) /*Eliminates repitition*/
+			{
+				putchar(hundreds);
+        putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && hundreds == '7' && tens == '8')) /*Adds commas and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

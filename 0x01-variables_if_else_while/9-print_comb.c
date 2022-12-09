7785eaf4fe4bf7d4cc0555fce0042ba60017d971
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2022-12-10 00:53:17 +0300

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 48 ; c < 58 ; c++)
+	{
+		putchar(c);
+    if (c != 57)
+    {
+      putchar(',');
+      putchar(' ');
+    }
+	}
+	putchar('\n');
+	return (0);
+}

1d7070ddd8c8a5f4e15f7ac0114723c58406e72d
MulukenMathewos <mulukenmathewos707@gmail.com>
2022-12-09 14:50:52 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,28 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-  int hundreds = '0';
+	int hundreds = '0';
 
-	for (hundreds = '0' ; hundreds <= '9' ; hundreds++) /*prints tens digit*/
+	for (hundreds = '0' ; hundreds <= '9' ; hundreds++) /*prints hundreds digit*/
 	{
-    for (tens = '0' ; tens <= '9' ; tens++) /*prints tens digit*/
-	{
-		for (ones = '0' ; ones <= '9' ; ones++) /*prints ones digit*/
+		for (tens = '0' ; tens <= '9' ; tens++) /*prints tens digit*/
 		{
-			if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))) /*Eliminates repitition*/
+			for (ones = '0' ; ones <= '9' ; ones++) /*prints ones digit*/
 			{
-				putchar(hundreds);
-        putchar(tens);
-				putchar(ones);
-				if (!(ones == '9' && hundreds == '7' && tens == '8')) /*Adds commas and space*/
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))) /*Eliminates repitition*/
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && tens == '8')) /*Adds commas and space*/
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
+		putchar('\n');
+		return (0);
 	}
-	putchar('\n');
-	return (0);
 }

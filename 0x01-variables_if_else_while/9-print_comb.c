ad14d52b5007df6bf2fc1778d34512d7166cc4de
MulukenMathewos <mulukenmathewos707@gmail.com>
2022-12-09 13:56:01 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 	for (c = 48 ; c < 58 ; c++)
 	{
 		putchar(c);
-    if (c != 57)
-    {
-      putchar(',');
-      putchar(' ');
-    }
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);

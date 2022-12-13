0558fbf21f055eb29e11109bb38d62b7fe8a0f7c
MulukenMathewos <mulukenmathewos707@gmail.com>
2022-12-13 08:08:44 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,18 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-    _putchar('0');
+		_putchar('0');
 		for (malt = 1; malt <= 9; malt++)
 		{
 			_putchar(',');
 			_putchar(' ');
-      prod = num * mult;
-      if (prod <= 9)
-          _putchar(' ');
-      else
-          _putchar((prod / 10) + '0');
+			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
 		}
-    _putchar('\n');
+		_putchar('\n');
 	}
 }

8397c8ab81e93633a91f29355bf7faf81d47f28c
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2022-12-16 11:41:18 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,10 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 48; c < 58; c++)
 	{
-		if (c == 2 || c == 4)
-			continue;
-		_putchar(c);
+		if (c != 50 || c != 52)
+			_putchar(c);
 	}
 	_putchar('\n');
 }

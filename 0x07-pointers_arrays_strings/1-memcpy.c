0330bc05f4c117e0aeb908c9fc565c9da734cfdc
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-01-02 11:19:29 -0800

    lol

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**                                                                                                                                     
+/**
  * *_memcpy -  copies memory area from src to dest
  * @dest: destination
  * @src: source
@@ -9,12 +9,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; n > 0 ; i++, n--)
-        {
-                dest[i] = src[i];
-        }
-  
-        return (dest);
+	for (i = 0; n > 0 ; i++, n--)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-        int index;
-  
-        for (index = 0; s[index] >= '\0'; index++)
-        {
-                if (s[index] == c)
-                        return (s + index);
-        }
-  
-        return ('\0');
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
 }

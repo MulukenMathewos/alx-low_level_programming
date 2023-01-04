4e0f9e1744017206be2d1cd716dda7329db69dee
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-01-04 14:10:44 -0800

    LoL

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,12 +12,12 @@ int is_palindrome(char *s);
  */
 int find_strlen(char *s)
 {
-        int len = 0;
-  
-        if (*(s + len))
-        {
-                len++;
-                len += find_strlen(s + len);
-        }
-        return (len);
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
 }

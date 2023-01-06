14aece2315df0f8a5c0e57c4bf9406e31c633afb
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-01-06 11:34:11 -0800

    l

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 void _puts(char *str)
-#include<stdio.h>
+
 /**
  * main - Entry point
- *
+ * _puts - prints a string, followed by a new line, to stdout
  * Return: Always 0 (Success)
  */
 int main(void)

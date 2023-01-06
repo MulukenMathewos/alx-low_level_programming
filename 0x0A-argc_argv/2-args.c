ce7d627f834dbf90c75e5e5e28decf61adf69150
MulukenMathewos <mulukenmathewos707@gmail.com>
2023-01-06 14:41:22 -0800

    lol

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,10 +7,10 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])	
+int main(int argc, char *argv[])
 {
 	int i;
-  
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
